69e5310bdf8270f4b9e39bb57d2927cafb3e5d1b
Yifei Chen <yifeichen@163.com>
2020-02-15 05:01:31 -0800

    Update time manager.py

--- time manager.py
+++ time manager.py
@@ -1,5 +1,5 @@
 
-def timetableGenerator(dict):
+def timetableGenerator(dict, after, before):
     
     timetables = []
 
@@ -10,33 +10,35 @@ def timetableGenerator(dict):
         times = dict[course]
 
         #expend timetables with possible time periods for this course
-        if size == 0:
+        if course == list(dict.keys())[0]:
             for time in times:
-                course_time = (course, time)
-                timetables.append([course_time])
-        else:
+                if filter(time, after, before):
+                    course_time = (course, time)
+                    timetables.append([course_time])
+
+        elif(size != 0):
             for i in range(0, size):
                 temp = timetables[0]
                 del(timetables[0])
                 for time in times:
-                    course_time = (course, time)
-                    timetables.append(temp + [course_time])
+                    if filter(time, after, before):
+                        course_time = (course, time)
+                        if(checkNewAddedItem(temp + [course_time])):
+                            timetables.append(temp + [course_time])
 
-        size = len(timetables)
-        
-        #Delete the timetables with time conflict
-        for i in range(0,size):
-            temp = timetables[0]
-            del(timetables[0])
-            if(checkNewAddedClass(temp)):
-                timetables.append(temp)
-        
         size = len(timetables)
 
     return timetables
 
-#Check if last added classes have time conflict in any one of the class in the timetable
-def checkNewAddedClass(x):
+def filter(c, after, before):
+    for t in c:
+        if (t[1] < after or t[2] > before):
+            return False
+    return True
+
+
+#Check if last added item have conflict in any one of the previous items in the list
+def checkNewAddedItem(x):
     for i in range(0, len(x)-1):
         if(not checkTwoClasses(x[i], x[-1])):
             return False
@@ -52,11 +54,11 @@ def checkTwoClasses(c1, c2):
     return True
 
 courseList = {}
-courseList = {"CPSC420": [[(1,13,14),(3,13,14),(5,13,14)],[(2,13,14.5),(4,13,14.5)]]}
+courseList = {"CPSC420": [[(1,13,14),(3,13,14),(5,13,14)],[(2,14.5,15),(4,14.5,15)]]}
 courseList["CPSC322"] = [[(1,13,14),(3,13,14),(5,13,14)],[(1,14,15),(3,14,15),(5,14,15)]]
 courseList["COGS300"] = [[(1,15,16),(3,15,16)]]
 
-timetable = timetableGenerator(courseList)
+timetable = timetableGenerator(courseList, 14, 15)
 
 print(timetable)
 
